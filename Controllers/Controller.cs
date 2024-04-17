e5d5c7dc7805890d309ab5ff7977650b039dc494
ErNeRooo <erneroooooo@gmail.com>
2024-04-17 19:40:40 +0200

    add GetAverage method

--- Controllers/Controller.cs
+++ Controllers/Controller.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using HtmlAgilityPack;
 
 namespace repmAPI.Controllers
@@ -7,10 +7,30 @@ namespace repmAPI.Controllers
     [Route("[controller]")]
     public class Controller : ControllerBase
     {
+        private string url = "https://warszawa.nieruchomosci-online.pl/szukaj.html?3,mieszkanie,wynajem,,Olsztyn:18670&o=price,desc";
+
         [HttpGet()] 
-        public ActionResult Get()
+        public ActionResult GetAverage()
         {
-            return Ok("Pozdro");
+            var client = new HttpClient();
+            var html = client.GetStringAsync(url).Result;
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var pricesElements = htmlDocument.DocumentNode.SelectNodes("//p[@class='title-a primary-display']/span[1]");
+
+            var prices = new List<int>();
+
+            foreach ( var priceElement in pricesElements )
+            {
+                string priceString = priceElement.InnerText.Replace("&nbsp;", "").Replace("zł", "");
+                int price = Convert.ToInt32(priceString);
+                prices.Add(price);
+            }
+
+            int pricesAverage = prices.Sum() / prices.Count;
+
+            return Ok(pricesAverage);
         }
     }
 }
