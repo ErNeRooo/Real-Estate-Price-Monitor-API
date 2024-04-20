c186aa81ef87828dcd482cba97486a35e5f65997
ErNeRooo <erneroooooo@gmail.com>
2024-04-20 15:14:33 +0200

    refactor GetAverage function

--- Controllers/Controller.cs
+++ Controllers/Controller.cs
@@ -7,30 +7,12 @@ namespace repmAPI.Controllers
     [Route("[controller]")]
     public class Controller : ControllerBase
     {
-        private string url = "https://warszawa.nieruchomosci-online.pl/szukaj.html?3,mieszkanie,wynajem,,Olsztyn:18670&o=price,desc";
+        
 
         [HttpGet()] 
         public ActionResult GetAverage()
         {
-            var client = new HttpClient();
-            var html = client.GetStringAsync(url).Result;
-            var htmlDocument = new HtmlDocument();
-            htmlDocument.LoadHtml(html);
-
-            var pricesElements = htmlDocument.DocumentNode.SelectNodes("//p[@class='title-a primary-display']/span[1]");
-
-            var prices = new List<int>();
-
-            foreach ( var priceElement in pricesElements )
-            {
-                string priceString = priceElement.InnerText.Replace("&nbsp;", "").Replace("zł", "");
-                int price = Convert.ToInt32(priceString);
-                prices.Add(price);
-            }
-
-            int pricesAverage = prices.Sum() / prices.Count;
-
-            return Ok(pricesAverage);
+            
         }
     }
 }
---
+++ Services/ScrapingService.cs
@@ -0,0 +1,44 @@
+﻿using HtmlAgilityPack;
+using System;
+
+namespace repmAPI.Services
+{
+    public class ScrapingService
+    {
+        private string url = "https://warszawa.nieruchomosci-online.pl/szukaj.html?3,mieszkanie,wynajem,,Olsztyn:18670&o=price,desc";
+        private HtmlDocument HtmlDocument;
+        public ScrapingService() 
+        {
+            LoadDocument();
+        }
+        private void LoadDocument()
+        {
+            var client = new HttpClient();
+            var html = client.GetStringAsync(url).Result;
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            HtmlDocument = htmlDocument;
+        }
+        public List<int> GetPrices()
+        {
+            var priceElements = HtmlDocument.DocumentNode.SelectNodes("//p[@class='title-a primary-display']/span[1]");
+
+            var prices = new List<int>();
+
+            foreach (var priceElement in priceElements)
+            {
+                string priceString = priceElement.InnerText.Replace("&nbsp;", "").Replace("zł", "");
+                int price = Convert.ToInt32(priceString);
+                prices.Add(price);
+            }
+
+            return prices;
+        }
+        public int GetAverage()
+        {
+            var prices = GetPrices();
+            return prices.Sum() / prices.Count;
+        }
+    }
+}
