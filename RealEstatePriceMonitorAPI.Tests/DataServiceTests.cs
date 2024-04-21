4a4e7bb2cac862a2e28507e6dc169a92311b7265
ErNeRooo <erneroooooo@gmail.com>
2024-04-21 10:54:51 +0200

    Add CalculateMedian_ForGivenNumbers_ReturnCorrectMedian Unit test

--- RealEstatePriceMonitorAPI.Tests/DataServiceTests.cs
+++ RealEstatePriceMonitorAPI.Tests/DataServiceTests.cs
@@ -5,7 +5,7 @@ namespace RealEstatePriceMonitorAPI.Tests
     public class DataServiceTests
     {
         [Theory]
-        [MemberData(nameof(Data))]
+        [MemberData(nameof(DataForCalculateAverage))]
         public void CalculateAverage_ForGivenNumbers_ReturnCorrectAverage(List<int> prices, int expectedResult)
         {
             //arrange
@@ -18,13 +18,43 @@ namespace RealEstatePriceMonitorAPI.Tests
             Assert.Equal(expectedResult, result);
         }
 
-
-        public static IEnumerable<object[]> Data()
+        [Theory]
+        [MemberData(nameof(DataForCalculateMedian))]
+        public void CalculateMedian_ForGivenNumbers_ReturnCorrectMedian(List<int> prices, int expectedResult)
         {
-            yield return new object[] { new List<int>() { 2, 3, 4 }, 3 };
+            //arrange
+            var dataService = new DataService();
+
+            //act
+            int result = dataService.CalculateMedian(prices);
+
+            //assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> DataForCalculateAverage()
+        {
+            yield return new object[] { new List<int>() { 0 }, 0 };
+            yield return new object[] { new List<int>() { 5 }, 5 };
+            yield return new object[] { new List<int>() { 2, 3 }, 2 };
+            yield return new object[] { new List<int>() { 3, 6, 9 }, 6 };
+            yield return new object[] { new List<int>() { 5, 10, 15, 20 }, 12 };
+            yield return new object[] { new List<int>() { 1, 1, 9, 2 }, 3 };
+            yield return new object[] { new List<int>() { 4, 2, 8, 34, 2, 1923 }, 328 };
+            yield return new object[] { new List<int>() { 1914, 1939, 2036, 0, 0, 0 }, 981 };
+        }
+        public static IEnumerable<object[]> DataForCalculateMedian()
+        {
+            yield return new object[] { new List<int>() { 0 }, 0 };
+            yield return new object[] { new List<int>() { 2 }, 2 };
+            yield return new object[] { new List<int>() { 2, 4 }, 3 };
             yield return new object[] { new List<int>() { 5, 10, 15 }, 10 };
             yield return new object[] { new List<int>() { 3, 6, 9 }, 6 };
+            yield return new object[] { new List<int>() { 2, 4, 4, 4, 6 }, 4 };
+            yield return new object[] { new List<int>() { 2, 4, 4, 2077, 4, 6 }, 4 };
+            yield return new object[] { new List<int>() { 0, 4, 4, 2077, 20, 6 }, 5 };
+            yield return new object[] { new List<int>() { 1, 2, 1233124, 141414, 4 , 13 }, 8 };
         }
-        
+
     }
 }
